0c193f58e92e0b2cc20ed5d9421d2c373d0d1674
Graham Ollis <plicease@cpan.org>
2015-08-11 10:00:54 -0400

    fix example

--- examples/spinning_cube.pl
+++ examples/spinning_cube.pl
@@ -8,16 +8,25 @@ use warnings;
 use 5.014;
 use OpenGL::FFI qw( :gl :glu :glut );
 
+my $x_rotated = 0;
+my $y_rotated = 0;
+my $z_rotated = 0;
+
 glutInit(@ARGV);
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 glutInitWindowSize(300, 300);
 glutCreateWindow("Cube example");
 glLightfv(GL_LIGHT0, GL_DIFFUSE, [1.0, 0.0, 0.0, 1.0]);
 glLightfv(GL_LIGHT0, GL_POSITION, [1.0, 1.0, 1.0, 0.0]);
-
-my $x_rotated = 0;
-my $y_rotated = 0;
-my $z_rotated = 0;
+glEnable(GL_LIGHT0);
+glEnable(GL_LIGHTING);
+glEnable(GL_CULL_FACE);
+glShadeModel(GL_SMOOTH);
+glClearColor(0.0, 0.0, 0.0, 0.0);
+glutDisplayFunc(\&display);
+glutIdleFunc(\&idle);
+glutReshapeFunc(\&reshape);
+glutMainLoop();
 
 sub display {
   glClear(GL_COLOR_BUFFER_BIT);
@@ -49,8 +58,3 @@ sub reshape {
   glViewport(0, 0, $x, $y);
 }
 
-glutDisplayFunc(\&display);
-glutIdleFunc(\&idle);
-glutReshapeFunc(\&reshape);
-
-glutMainLoop();
